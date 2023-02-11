ff50b0eb6d6a0f352b07faf6803e272e8453e7f0
James Swerdfeger <jswerdfeger@gmail.com>
2023-02-10 22:00:08 -0500

    Add QueueExtensions

---
+++ src/QueueExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace EagerAsyncEnumerable;
+
+/// <summary>
+/// Provides extensions to <see cref="Queue"/>.
+/// </summary>
+internal static class QueueExtensions
+{
+#if NETSTANDARD2_0
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	internal static bool TryDequeue<T>(this Queue<T> queue, out T result)
+	{
+		if (queue.Count == 0)
+		{
+			result = default!;
+			return false;
+		} 
+		else
+		{
+			result = queue.Dequeue();
+			return true;
+		}
+	}
+#endif
+
+}
